b134f12840ee764c3f1a2a440ba1f5cbb1ef1eea
Lucas Sutton <lucas109ss@outlook.com>
2025-11-02 13:40:28 -0500

    Fix

--- include/timestamp.h
+++
@@ -1,6 +0,0 @@
-#ifndef TIMESTAMP_H
-#define TIMESTAMP_H
-
-long long current_timestamp_microseconds(void);
-
-#endif // TIMESTAMP_H
--- src/timestamp.c
+++
@@ -1,9 +0,0 @@
-#include "timestamp.h"
-
-#include <sys/time.h>
-
-long long current_timestamp_microseconds(void) {
-    struct timeval te;
-    gettimeofday(&te, NULL);
-    return (long long)te.tv_sec * 1000000LL + te.tv_usec;
-}
